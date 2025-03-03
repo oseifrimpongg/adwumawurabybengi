b1d15db3375cd480b61cc4665aba3df9c9d7287d
Michael Osei Frimpong <michaelfrimpong543@gmail.com>
2025-03-03 02:32:10 +0000

    feature: implemented queries assembly and changed user logic
    Now queries are automatically hooked from IQueryHandler
    
    Users are also automatically saved to the db once they interact with the bot

--- src/Program.cs
+++ src/Program.cs
@@ -6,9 +6,8 @@ using System.Reflection;
 using Adwumawura.src.Commands;
 using Telegram.Bot.Types.Enums;
 using Adwumawura.src.Middleware;
-using MongoDB.Driver;
 using Adwumawura.src.Db;
-using MongoDB.Bson;
+using Adwumawura.Bot.src.QueryHandlers;
 
 // Variables Setup
 Env.Load();
@@ -21,7 +20,8 @@ string DatabaseName = Environment.GetEnvironmentVariable("DATABASE_NAME") ?? "";
 
 // Setup
 using CancellationTokenSource cts = new CancellationTokenSource();
-TelegramBotClient bot = new TelegramBotClient(token: BotToken, cancellationToken: cts.Token);
+// TelegramBotClient bot = new TelegramBotClient(token: BotToken, cancellationToken: cts.Token); // Production ready
+TelegramBotClient bot = new TelegramBotClient("7709986675:AAECwsyQwRTgw7gTvReOKistEZ64V4YfFYY", cancellationToken: cts.Token); // Testing
 
 Console.WriteLine($"Bot is active");
 
@@ -33,6 +33,15 @@ Dictionary<string, IBotCommand> commands = Assembly
                                              .Select(type => (IBotCommand)Activator.CreateInstance(type)!)
                                              .ToDictionary(cmd => cmd.Name, cmd => cmd);
 
+// Handle Queries
+Dictionary<string, IQueryHandler> queries = Assembly
+                                             .GetExecutingAssembly()
+                                             .GetTypes()
+                                             .Where(type => typeof(IQueryHandler).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+                                             .Select(type => (IQueryHandler)Activator.CreateInstance(type)!)
+                                             .ToDictionary(query => query.GetType().Name.Replace("ProgrammeQueryHandler", "p").ToLower(), query => query);
+
+
 ChannelSubscriptionMiddleware? channelMembershipStatus = new ChannelSubscriptionMiddleware(bot, Channel_Id);
 
 var mongoClient = new MongoDbClient(MongoDbConnectionString, DatabaseName);
@@ -41,39 +50,16 @@ var usersCollection = mongoClient.GetUsers();
 bot.StartReceiving(
    async (client, update, ct) =>
    {
+      // Subscription & User Data Handler
       bool isSubscribed = await channelMembershipStatus.CheckSubscription(update);
-      if (!isSubscribed) return;
-
-      User userUpdate = update.Message.From;
-      if (userUpdate is null) return;
-
-      var existingUser = usersCollection.Find(u => u.UserId == userUpdate.Id).FirstOrDefault();
-
-      if (existingUser is null)
+      if (!isSubscribed && usersCollection is not null)
       {
-         var user = new UserModel()
-         {
-            Id = ObjectId.GenerateNewId(),
-            UserId = userUpdate.Id,
-            UserName = userUpdate?.Username,
-            FirstName = userUpdate?.FirstName,
-            LastName = userUpdate?.LastName,
-            JoinDate = DateTime.Now,
-            SubscriptionStatus = isSubscribed
-         };
-
-         try
-         {
-            await usersCollection.InsertOneAsync(user);
-         }
-         catch (Exception exception)
-         {
-            Console.WriteLine($"${exception.Message}");
-         }
-
+         await mongoClient.SaveUserAsync(client, update, usersCollection);
+         return;
       }
 
-      if (update.Type == UpdateType.Message && update.Message != null)
+      // Message Handler
+      if (update?.Type == UpdateType.Message && update.Message != null)
       {
          string? command = update?.Message?.Text?.Split(" ")[0];
 
@@ -82,6 +68,21 @@ bot.StartReceiving(
             await commands[command].ExecuteCommand(update ?? new Update(), client);
          }
       }
+
+      // Callbacks Handler
+      if (update?.Type == UpdateType.CallbackQuery && update?.CallbackQuery != null)
+      {
+         string? callbackQueryData = update?.CallbackQuery?.Data;
+         if (string.IsNullOrEmpty(callbackQueryData)) return;
+
+         string queryKey = callbackQueryData.Split("/")[0];
+         if (queries.ContainsKey(queryKey) && update is not null)
+         {
+            await queries[queryKey].HandleQuery(client, update);
+         }
+         return;
+      }
+
    },
    (client, exception, ct) =>
    {
@@ -94,4 +95,3 @@ bot.StartReceiving(
 
 Console.ReadLine();
 cts.Cancel();
-
