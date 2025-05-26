b5d5ec99b1317f78c354ed7eb9538384ca4fd174
Michael Osei Frimpong <michaelfrimpong543@gmail.com>
2025-05-26 00:11:39 +0000

    chore: modify start command to automatically fill user information from the database

--- bot-ts/src/commands/start.ts
+++ bot-ts/src/commands/start.ts
@@ -1,9 +1,11 @@
 import { Context } from "telegraf";
+import User from "../models/User";
 
-export const startCommand = async (ctx: Context) =>
+export const StartCommand = async (ctx: Context) =>
 {
    const chatId = ctx.message != null ? ctx.message.chat.id : 694873497;
-   const text = `Hi! I am Adwumawura, you personal guide to all your course materials. Let's make studying easier and more fun and easier together!🎓\nFirst, choose your programme of study to get started. 🚀`;
+   const userInformation = await User.findOne({ telegramId: chatId });
+   const text = `Hi ${userInformation?.firstName}! I am Adwumawura, you personal guide to all your course materials. Let's make studying ${userInformation?.programme} easier and more fun and together!🎓🚀`;
 
    ctx.telegram.sendMessage(chatId, text);
 };
