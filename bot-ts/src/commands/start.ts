9354fc65fe0e9df546ad43c52134e887cc2bd1f2
Michael Osei Frimpong <michaelfrimpong543@gmail.com>
2025-05-27 13:33:31 +0000

    chore: update start command markup style

--- bot-ts/src/commands/start.ts
+++ bot-ts/src/commands/start.ts
@@ -1,32 +1,17 @@
-import { Context } from "telegraf";
+import { Context, Markup } from "telegraf";
 import User from "../models/User";
 
 export const StartCommand = async (ctx: Context) =>
 {
    const chatId = ctx.message != null ? ctx.message.chat.id : 694873497;
    const userInformation = await User.findOne({ telegramId: chatId });
-   const text = `Hi ${userInformation?.firstName}! I am Adwumawura, you personal guide to all your course materials. Let's make studying ${userInformation?.programme} easier and more fun and together!🎓🚀`;
+   const text = `Hi ${userInformation?.firstName}\\! I am Adwumawura, you personal guide to all your course materials\\. Let's make studying ${userInformation?.programme} easier and more fun and together\\!🎓🚀`;
 
-   return ctx.telegram.sendMessage(chatId, text,
-      {
-         reply_markup: {
-            keyboard: [
-               [{ text: "Course Materials 📙" }, { text: "Past Questions 🯄" }], [{ text: "AI Chat 🤖" }]
-            ],
-            one_time_keyboard: true,
-            resize_keyboard: true
-         }, parse_mode: "Markdown"
-      });
+   return ctx.replyWithMarkdownV2(text,
+      Markup.keyboard([
+         ["Course Materials 📙", "Past Questions 🯄"],
+         ["AI Chat 🤖"]
+      ]).oneTime().resize()
+   );
+
 };
-
-
-/*
-Course Materials,
-past questions
-ai chat
-
-each learning material should have three buttons,
-- summary button
-- quiz button
-- bookmark will come later
-*/
