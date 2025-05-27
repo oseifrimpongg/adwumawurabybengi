568bc5626dc58f084a36a7b472cc77e0cb795c7a
Michael Osei Frimpong <michaelfrimpong543@gmail.com>
2025-05-27 03:02:01 +0000

    chore: update start command return buttons

--- bot-ts/src/commands/start.ts
+++ bot-ts/src/commands/start.ts
@@ -7,5 +7,26 @@ export const StartCommand = async (ctx: Context) =>
    const userInformation = await User.findOne({ telegramId: chatId });
    const text = `Hi ${userInformation?.firstName}! I am Adwumawura, you personal guide to all your course materials. Let's make studying ${userInformation?.programme} easier and more fun and together!🎓🚀`;
 
-   ctx.telegram.sendMessage(chatId, text);
+   return ctx.telegram.sendMessage(chatId, text,
+      {
+         reply_markup: {
+            keyboard: [
+               [{ text: "Course Materials 📙" }, { text: "Past Questions 🯄" }], [{ text: "AI Chat 🤖" }]
+            ],
+            one_time_keyboard: true,
+            resize_keyboard: true
+         }, parse_mode: "Markdown"
+      });
 };
+
+
+/*
+Course Materials,
+past questions
+ai chat
+
+each learning material should have three buttons,
+- summary button
+- quiz button
+- bookmark will come later
+*/
