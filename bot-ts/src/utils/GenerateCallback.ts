d75e8f21d7b8fa32c976c1c4f1b87354a9a1dde1
Michael Osei Frimpong <michaelfrimpong543@gmail.com>
2025-05-28 01:50:56 +0000

    feature: generate function utils

---
+++ bot-ts/src/utils/GenerateCallback.ts
@@ -0,0 +1,22 @@
+import { Document } from "mongoose";
+import { IUser } from "../models/User";
+import { LearningMaterial, ICallbackData } from "../types/callbackTypes";
+
+export function GenerateCallback(requestType: LearningMaterial, semester: number, data: (Document<unknown, {}, IUser, {}> & IUser & Required<{ _id: unknown; }> & { __v: number; }) | null)
+{
+   if (data == null) return "";
+
+   const actionMatch = {
+      [LearningMaterial.Lecture]: "lec",
+      [LearningMaterial.Past_Questions]: "pas"
+   };
+
+   const returnData: ICallbackData = {
+      a: actionMatch[requestType],
+      s: semester,
+      y: data.year,
+      p: `${data?.programme.substring(0, 3)}`
+   };
+
+   return JSON.stringify(returnData);
+}
