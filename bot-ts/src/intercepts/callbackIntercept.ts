9a32db3b89fb77989c1e4954939809ae17626055
Michael Osei Frimpong <michaelfrimpong543@gmail.com>
2025-05-28 01:50:06 +0000

    feature: implement callback interceptions

--- bot-ts/src/intercepts/callbackIntercept.ts
+++ bot-ts/src/intercepts/callbackIntercept.ts
@@ -1,10 +1,25 @@
+import { ShowCourses } from "../services/courseMaterialService";
+import { ICallbackData } from "../types/callbackTypes";
 import { MyContext } from "../types/types";
 
-const callbackRoutes = {
+const callbackRoutes: [RegExp, (ctx: MyContext, previousPage: ICallbackData) => void][] = ([
+   [/^\{"a":"lec","s":\d+,"y":\d+,"p":"[A-Za-z]{3}"\}$/, ShowCourses]
+]);
 
-} as const;
-
-export const InterceptCallback = (ctx: MyContext) =>
+export const InterceptCallback = async (ctx: MyContext) =>
 {
+   const callback = ctx.callbackQuery;
+   if (!(callback == undefined) && !("data" in callback) || callback == undefined) return;
 
+   const callbackData: ICallbackData = JSON.parse(callback.data);
+   const callbackAsString: string = JSON.stringify(callbackData);
+
+   for (const [regex, route] of callbackRoutes)
+   {
+      if (regex.test(callbackAsString))
+      {
+         await route(ctx, callbackData);
+         return;
+      };
+   }
 };
